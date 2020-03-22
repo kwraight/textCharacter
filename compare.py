0f5066352db659478b064ec1bcdb548e6773ea31
kwraight <kenneth.gibb.wraight@gmail.com>
2020-03-22 23:20:40 +0000

    fill in skeleton

--- compare.py
+++ compare.py
@@ -3,7 +3,9 @@ import numpy as np
 import time
 import argparse
 import operator
-import character
+import pandas as pd
+import character as chr
+import radarPlot as rp
 
 
 # commandline arguments
@@ -42,13 +44,44 @@ def GetArgs():
 ### useful functions
 ######################################
 
-def GetCharacteristics():
+def GetCharacteristics(filename):
+# get characterisitic parameters from file
+
+    rawList= chr.GetRawList(filename) # read in all characters from file (remove new lines)
+    cleanList= chr.GetCleanList(rawList, [" ", "\\"]) # clean list of bad characters (e.g. whitespaces)
+    lowList= [ch.lower() for ch in cleanList] # make all characters lower case
+    freqDict = {i:lowList.count(i) for i in lowList} # count frequency for each unique character
+    mpv, popVowels= chr.GetMostPopular(freqDict, "aeiou")
+    mpc, popConsonants= chr.GetMostPopular(freqDict, "bcdfghjklmnpqrstvwxyz")
+    retDict= {
+            'group': filename,
+            'total': len(cleanList),
+            'uniques': len(freqDict),
+            'aveVows': chr.GetAveFreq(freqDict, "aeiou"),
+            'MPV': mpv,
+            'aveCons': chr.GetAveFreq(freqDict, "bcdfghjklmnpqrstvwxyz"),
+            'MPC': mpc,
+            'AWL': chr.GetAveWordLength(filename)
+            }
+    # all character output
+    print(freqDict)
+    print("total # characters:", retDict['total'])
+    print("total unique characters:", retDict['uniques'])
+    # average frequencies
+    print("average vowel frequency:", retDict['aveVows'])
+    print("average consonants frequency:", retDict['aveCons'])
+    # most popular characters
+    print("most popular vowels ("+str(mpv)+"):", popVowels)
+    print("most popular consonants ("+str(mpc)+"):", popConsonants)
+    # average word length
+    print("average word length:", retDict['AWL'])
+
+    return retDict
 
 
-    return
+def PlotComparison(df):
 
-def PlotComparison():
-
+    rp.MakeRadar(df)
 
     return
 
@@ -58,17 +91,57 @@ def PlotComparison():
 
 def main():
 
-get chactgerisitics per file
-plot PlotComparison
-table comparison
+    argDict=GetArgs()
+    print("argDict:",argDict)
 
+    '''
+    get chactgerisitics per file
+    plot PlotComparison
+    table comparison
+    '''
+
+    if len(argDict['infiles'])==0:
+        print("No input filenames specificed. Exiting")
+        return
+
+    dataDict = {
+    'group': [],
+    'uniques': [],
+    'AWL': [],
+    'aveVows': [],
+    'MPV': [],
+    'aveCons': [],
+    'MPC': []
+    }
+
+    for f in argDict['infiles']:
+        tempDict=GetCharacteristics(f)
+        print("### the second")
+        print(tempDict)
+        dataDict['group'].append(tempDict['group'])
+        dataDict['uniques'].append(tempDict['uniques'])
+        dataDict['AWL'].append(tempDict['AWL'])
+        dataDict['aveVows'].append(tempDict['aveVows'])
+        dataDict['MPV'].append(tempDict['MPV'])
+        dataDict['aveCons'].append(tempDict['aveCons'])
+        dataDict['MPC'].append(tempDict['MPC'])
+
+    df= pd.DataFrame.from_dict(dataDict)
+
+    # Plotting
+    if argDict['plot']==1:
+        PlotComparison(df)
+
+
+    print("### Comparison Table")
+    print(df[['group','uniques','AWL','aveVows','MPV','aveCons','MPC']].to_string(index=False))
 
     return
 
 if __name__ == "__main__":
-print "### in",__file__,"###"
-start = time.time()
-main()
-end = time.time()
-print "\n+++ Total scan time: ",(end-start),"seconds +++\n"
-print "### out",__file__,"###"
+    print "### in",__file__,"###"
+    start = time.time()
+    main()
+    end = time.time()
+    print "\n+++ Total scan time: ",(end-start),"seconds +++\n"
+    print "### out",__file__,"###"
